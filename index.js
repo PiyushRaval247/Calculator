5eed73f65a3f10357c95788e1549db81fdf0f7fc
PiyushRaval247 <124157203+PiyushRaval247@users.noreply.github.com>
2023-02-01 18:32:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let screen = document.getElementById('display');
+
+function show(n) {
+    display.value += n;
+}
+
+function allclr() {
+    display.value = "";
+}
+
+function res() {
+    display.value = eval(display.value);
+}
+
+function del() {
+    display.value = display.value.slice(0, -1);
+}
